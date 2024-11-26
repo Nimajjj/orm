aff95b890a657a5cec34f5a3c70b968e9b9c7c05
Nimajjj <benjamin.borello@aol.com>
2024-11-26 17:32:59 +0100

    add update and delete

--- src/NewsEntityManager.php
+++ src/NewsEntityManager.php
@@ -37,11 +37,37 @@ final class NewsEntityManager
 
     public function update(News $news): News
     {
+        $query = (new QueryBuilder())
+            ->buildAction(QueryAction::UPDATE)
+            ->buildTable("News")
+            ->buildColumns(["id", "content", "created_at"])
+            ->buildValues([$news->getId(), $news->getContent(), $news->getCreatedAt()])
+            ->buildCondition("id", QueryCondition::IS_EQUAL, $news->getId()->getValue())
+            ->build();
 
+        $adapter = new MySQLAdapter(); // TODO : pass adapter in private attribute
+
+        $__ = [];
+        $result = $adapter->executeQuery($query, $__);
+
+        assert(!$result, "Querry failed.");
+
+        return $news;
     }
 
     public function delete(News $news): void
     {
+        $query = (new QueryBuilder())
+            ->buildAction(QueryAction::DELETE)
+            ->buildTable("News")
+            ->buildCondition("id", QueryCondition::IS_EQUAL, $news->getId()->getValue())
+            ->build();
 
+        $adapter = new MySQLAdapter(); // TODO : pass adapter in private attribute
+
+        $__ = [];
+        $result = $adapter->executeQuery($query, $__);
+
+        assert(!$result, "Querry failed.");
     }
 }
